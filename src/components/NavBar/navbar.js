994d2f18fe0b8c0ab86d3edb100dc765cf21ae18
Mike Rodriguez <devmiker@gmail.com>
2022-06-20 14:28:06 -0400

    add navbar toggle

--- src/components/NavBar/navbar.js
+++ src/components/NavBar/navbar.js
@@ -1,35 +1,51 @@
-import './navbar.css';
-import { FaCode } from 'react-icons/fa';
-import MenuIcon from '../../assets/images/icons8-menu.json';
+import { FaCode } from "react-icons/fa";
+import React, { useState, useEffect } from "react";
+import "./navbar.css";
 
 const Navbar = () => {
-    return (
-        <div className='nav-menu'>
-            <div className='nav'>
-                <span className='nav-logo'><FaCode size='1.6em' alt='Source Code Icon' /></span>
-                <ul className='nav-ul'>
-                    <li className='nav-ul-li'><a href='#my-services'>&#60;Services /&#62;</a></li>
-                    <li className='nav-ul-li'><a href='#aboutme'>&#60;About Me /&#62;</a></li>
-                    <li className='nav-ul-li'><a href='#projects'>&#60;Projects /&#62;</a></li>
-                    <li className='nav-ul-li'><a href='#footer'>&#60;Social Media /&#62;</a></li>
-                </ul>
-            </div>
-            <div className='res-menu'>
-                <button 
-                        style={{
-                            color: 'black',
-                            backgroundColor: 'rgb(70, 175, 115)',
-                            border: 'none',
-                            padding: '0',
-                            margin: '0 30px',
-                            fontSize: '40px',
-                        }}
-                        >
-                    <img src={MenuIcon} alt='Drop down menu icon'/>
-                </button>
-            </div>
-        </div>
-    )
-}
+  const [toggleMenu, setToggleMenu] = useState(false);
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+
+  const toggleNav = () => {
+    setToggleMenu(!toggleMenu);
+  };
+
+  useEffect(() => {
+    const changeWidth = () => {
+      setScreenWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", changeWidth);
+
+    return () => {
+      window.removeEventListener("resize", changeWidth);
+    };
+  }, []);
+  return (
+    <div className="nav-menu">
+      <nav className="nav">
+        {(toggleMenu || screenWidth > 910) && (
+          <ul className="nav-ul">
+            <li className="nav-ul-li">
+              <a href="#my-services">&#60;Services /&#62;</a>
+            </li>
+            <li className="nav-ul-li">
+              <a href="#aboutme">&#60;About Me /&#62;</a>
+            </li>
+            <li className="nav-ul-li">
+              <a href="#projects">&#60;Projects /&#62;</a>
+            </li>
+            <li className="nav-ul-li">
+              <a href="#footer">&#60;Social Media /&#62;</a>
+            </li>
+          </ul>
+        )}
+        <span onClick={toggleNav} className="nav-logo">
+          <FaCode size="1.4em" alt="Source Code Icon" />
+        </span>
+      </nav>
+    </div>
+  );
+};
 
 export default Navbar;
